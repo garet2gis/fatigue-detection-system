4968c91b398a2bfa18c41e9adda8c56439ecb4f2
Daniil Mukovskiy <dm@modumlab.com>
2024-05-25 13:50:13 +0300

    user_data_service[docs]: add comments to business logic

--- user_data_service/internal/handlers/auth.go
+++ user_data_service/internal/handlers/auth.go
@@ -17,7 +17,7 @@ import (
 
 // Register godoc
 //
-//	@Summary	Принимает данные пользователя
+//	@Summary	Принимает данные пользователя и регистрирует его
 //	@ID			register
 //	@Tags		auth
 //	@Param		user_data	body	fixtures.RegisterRequest	true	"Данные для регистрации"
@@ -25,21 +25,25 @@ import (
 //	@Failure	400	{object}	app_errors.AppError
 //	@Router		/auth/register [post]
 func (c *CoreHandler) Register(w http.ResponseWriter, r *http.Request) error {
+	// объявляем текущую операцию для оборачивания ошибки
 	op := "handlers.CoreHandler.Register"
+	// берем логгер из контекста
 	l := logger.EntryWithRequestIDFromContext(r.Context())
 
+	// десереализуем данные из тела запроса
 	var req fixtures.RegisterRequest
-
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		return app_errors.ErrParseError.WrapError(op, err.Error())
 	}
 
+	// валидируем данные на наличие необходимых полей
 	appErr := customTools.ValidateStruct(c.validator, req)
 	if appErr != nil {
 		return appErr
 	}
 
+	// создаем нового пользователя в БД
 	_, err = c.authRepository.CreateUser(r.Context(), auth.User{
 		Name:         req.Name,
 		Surname:      req.Surname,
@@ -50,6 +54,7 @@ func (c *CoreHandler) Register(w http.ResponseWriter, r *http.Request) error {
 		return fmt.Errorf("%s: %w", op, err)
 	}
 
+	// Возвращаем пустой ответ со статусом 204
 	api.WriteSuccess(r.Context(), w, struct{}{}, http.StatusNoContent, l)
 
 	return nil
@@ -65,71 +70,81 @@ func (c *CoreHandler) Register(w http.ResponseWriter, r *http.Request) error {
 //	@Failure	400					{object}	app_errors.AppError
 //	@Router		/auth/login [post]
 func (c *CoreHandler) Login(w http.ResponseWriter, r *http.Request) error {
+	// объявляем текущую операцию для оборачивания ошибки
 	op := "handlers.CoreHandler.Login"
+	// берем логгер из контекста
 	l := logger.EntryWithRequestIDFromContext(r.Context())
 
+	// десереализуем данные из тела запроса
 	var req fixtures.LoginRequest
-
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		return app_errors.ErrParseError.WrapError(op, err.Error())
 	}
 
+	// валидируем данные на наличие необходимых полей
 	appErr := customTools.ValidateStruct(c.validator, req)
 	if appErr != nil {
 		return appErr
 	}
 
+	// находим пользователя в БД по логину
 	user, err := c.authRepository.GetUserByLogin(r.Context(), req.Login)
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
 
+	// сравниваем захэшированный пароль, хранящийся в БД с паролем в запросе
 	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(req.Password))
 	if err != nil {
 		return app_errors.ErrUnauthorized
 	}
 
+	// создаем JWT токен доступа
 	tokenString, err := c.tokenGenerator.CreateAccessToken(r.Context(), user.UserID)
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
 
+	// делаем запрос в сервис работы с моделями, чтобы в результате получить ссылки на их скачивание
 	models, err := c.getModels(user.UserID)
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
 
+	// формируем ответ на запрос
 	res, err := fixtures.NewLoginResponse(user.UserID, c.BaseURL, tokenString, models)
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
 
+	// возвращаем сформированный ответ
 	api.WriteSuccess(r.Context(), w, res, http.StatusOK, l)
 
 	return nil
 }
 
 func (c *CoreHandler) getModels(userID string) (map[string]interface{}, error) {
+	// объявляем текущую операцию для оборачивания ошибки
 	op := "handlers.CoreHandler.getModels"
 	requestBody := map[string]string{
 		"user_id": userID,
 	}
 
-	// Кодируем нашу структуру данных в JSON
+	// кодируем нашу структуру данных в JSON
 	jsonData, err := json.Marshal(requestBody)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	// Создаем новый запрос POST с JSON в качестве тела запроса
+	// создаем новый запрос POST с JSON в качестве тела запроса
 	req, err := http.NewRequest("POST", c.StorageURL, bytes.NewBuffer(jsonData))
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 	req.Header.Set("Content-Type", "application/json")
 
-	// Отправляем запрос
+	// отправляем запрос
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -137,19 +152,20 @@ func (c *CoreHandler) getModels(userID string) (map[string]interface{}, error) {
 	}
 	defer resp.Body.Close()
 
-	// Читаем ответ
+	// читаем ответ
 	responseData, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	// Десериализуем JSON ответ в map[string]string
+	// десериализуем JSON ответ в map[string]string
 	var result map[string]interface{}
 	err = json.Unmarshal(responseData, &result)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
+	// возвращаем в ответ ссылки на модели, если они есть
 	models, ok := result["content"].(map[string]interface{})
 	if ok {
 		_, ok = models["face_model"].(string)
@@ -158,5 +174,6 @@ func (c *CoreHandler) getModels(userID string) (map[string]interface{}, error) {
 		}
 	}
 
+	// возвращаем пустую структуру
 	return result, nil
 }
