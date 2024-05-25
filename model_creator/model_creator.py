b4db0dcb5cbf5dfebd8d007394a692bd528ad1a6
Daniil Mukovskiy <dm@modumlab.com>
2024-05-25 13:52:29 +0300

    model_creator[docs]: add comments to business logic

--- model_creator/model_creator.py
+++ model_creator/model_creator.py
@@ -7,38 +7,28 @@ import logging
 import os
 import requests
 
-
+# create_xgb - функция создания модели градиентного бустинга
 def create_xgb(data, url, user_id, model_type, features_count):
+    # Удаляем ненужные в обучении колонки
     data = data.drop(columns=['video_id'])
     data = data.drop(columns=['frame_count'])
     data = data.drop(columns=['user_id'])
 
+    # берем числовые колонки
     df_num = data.select_dtypes(include=[np.number])
-    df_cat = data.select_dtypes(include=[object])
     num_cols = df_num.columns.values[:-1]
-
-    for col in num_cols:
-        Q1, Q3 = data.loc[:, col].quantile([0.25, 0.75]).values
-        IQR = Q3 - Q1
-        box_max = Q3 + (1.5 * IQR)
-        box_min = Q1 - (1.5 * IQR)
-        data.loc[data[col] < box_min, col] = np.NaN
-        data.loc[data[col] > box_max, col] = np.NaN
-
-    for col in num_cols:
-        cur_mean = np.mean(data[col])
-        data[col] = data[col].fillna(cur_mean)
-
-    data.dropna(inplace=True, axis=0)
-
     df = data[num_cols]
 
+    # определяем дата-фрейм признаков и соответствующих им классов
     X = df
     Y = data["label"]
 
+    # разделяем данные на тестовые и тренировочные
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
+    # задаем гиперпараметры
     xgb_best_params = {'colsample_bytree': 0.8, 'eta': 0.1, 'max_depth': 9, 'n_estimators': 800, 'subsample': 0.7}
+    # объявляем и обучаем модель
     xgb = xgboost.XGBClassifier(**xgb_best_params)
     xgb.fit(X_train, y_train)
 
@@ -50,12 +40,15 @@ def create_xgb(data, url, user_id, model_type, features_count):
 
     file_path = './models/tmp.xgb'
     xgb.save_model(file_path)
+    # Отправялем модель в сервис работы с моделями
     send_model(file_path, url, user_id, model_type, features_count)
+    # Удаляем модель
     delete_file(file_path)
 
-
+# delete_file - функция удаления файла
 def delete_file(file_path):
     try:
+        # если файл существует, то удаляем его
         if os.path.exists(file_path):
             os.remove(file_path)
             logging.info("Файл успешно удален с диска.")
@@ -64,18 +57,22 @@ def delete_file(file_path):
     except Exception as e:
         logging.error(f"Произошла ошибка при удалении файла: {str(e)}")
 
-
+# send_model - функция отправления файла модели
 def send_model(file_path, url, user_id, model_type, features_count):
     try:
+        # открываем файл обученной модели
         with open(file_path, 'rb') as file:
+            # задаем строковые поля формы
             data = {
                 'user_id': user_id,
                 'model_type': model_type,
                 'features_count': str(features_count)
             }
-
+            # задаем файл модели в поле file
             files = {'file': file}
+            # отправляем http-запросом модель и другие данные
             response = requests.post(url, data=data, files=files)
+            #  логируем успешность выполненного запроса
             if str(response.status_code).startswith('2'):
                 logging.info(f"Файл успешно отправлен по HTTP: {file_path}")
             else:
