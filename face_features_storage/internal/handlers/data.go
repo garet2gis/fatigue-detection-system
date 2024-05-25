2eda6c09aa38815c160ab2de17ce57c59ab1530b
Daniil Mukovskiy <dm@modumlab.com>
2024-05-25 13:49:45 +0300

    face_features_storage[docs]: add comments to business logic

--- face_features_storage/internal/handlers/data.go
+++ face_features_storage/internal/handlers/data.go
@@ -21,17 +21,22 @@ import (
 //	@Failure	400	{object}	app_errors.AppError
 //	@Router		/face_model/save_features [post]
 func (c *CoreHandler) SaveVideoFeatures(w http.ResponseWriter, r *http.Request) error {
+	// объявляем текущую операцию для оборачивания ошибки
 	op := "handlers.CoreHandler.SaveVideoFeatures"
-
+	// берем логгер из контекста
 	l := logger.EntryWithRequestIDFromContext(r.Context())
 
+	// берем файл из формы по ключу file
 	file, _, err := r.FormFile("file")
 	if err != nil {
 		return err
 	}
+	// берем строковое значение user_id из формы
 	userID := r.FormValue("user_id")
+	// берем строковое значение model_type из формы
 	modelType := r.FormValue("model_type")
 
+	// закрываем файл при выходе из функции
 	defer func(file multipart.File) {
 		err := file.Close()
 		if err != nil {
@@ -39,13 +44,15 @@ func (c *CoreHandler) SaveVideoFeatures(w http.ResponseWriter, r *http.Request) 
 		}
 	}(file)
 
+	// делаем сохранение данных в транзакции
 	txErr := c.transactor.WithinTransaction(r.Context(), func(txCtx context.Context) error {
 		var featuresCount uint64
+		// сохраняем данные файла в таблице признаков
 		featuresCount, err = c.dataRepository.SaveFaceVideoFeatures(txCtx, file)
 		if err != nil {
 			return fmt.Errorf("%s: %w", op, err)
 		}
-
+		// отправляем количество сохраненных признаков в сервис работы с моделями
 		err = c.sendFeaturesCount(userID, modelType, int(featuresCount))
 		if err != nil {
 			return fmt.Errorf("%s: %w", op, err)
@@ -53,11 +60,11 @@ func (c *CoreHandler) SaveVideoFeatures(w http.ResponseWriter, r *http.Request) 
 
 		return nil
 	})
-
 	if txErr != nil {
 		return txErr
 	}
 
+	// Возвращаем пустой ответ со статусом 204
 	api.WriteSuccess(r.Context(), w, struct{}{}, http.StatusNoContent, l)
 	return nil
 }
@@ -69,36 +76,43 @@ type IncreaseFeaturesRequest struct {
 }
 
 func (c *CoreHandler) sendFeaturesCount(userID, modelType string, featuresCount int) error {
+	// объявляем текущую операцию для оборачивания ошибки
 	op := "handlers.CoreHandler.sendFeaturesCount"
 
+	// определяем тело запроса
 	req := IncreaseFeaturesRequest{
 		ModelType:     modelType,
 		UserID:        userID,
 		FeaturesCount: featuresCount,
 	}
 
+	// сериализуем тело запроса в JSON
 	jsonData, err := json.Marshal(req)
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
 
+	// создаем запрос по соответствующему url и кладем в него тело запроса
 	r, err := http.NewRequest("POST", c.storageHandlerURL, bytes.NewBuffer(jsonData))
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
 
+	// задаем тип контента равным application/json
 	r.Header.Set("Content-Type", "application/json")
 
-	// Создаем клиент HTTP для отправки запроса
+	// cоздаем клиент HTTP для отправки запроса
 	client := &http.Client{}
 
-	// Отправляем запрос
+	// отправляем запрос
 	resp, err := client.Do(r)
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
+	// закрываем тело ответа при выходе из функции
 	defer resp.Body.Close()
 
+	// в случае ошибочного статуса ответа возвращаем ошибку
 	if resp.StatusCode != http.StatusNoContent {
 		return fmt.Errorf("%s: error from model_handler service", op)
 	}
