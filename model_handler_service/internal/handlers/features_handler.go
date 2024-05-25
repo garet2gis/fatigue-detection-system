ee8da175b3274007a34aee05308ebf161b0653d4
Daniil Mukovskiy <dm@modumlab.com>
2024-05-25 13:51:14 +0300

    model_handler_service[docs]: add comments to business logic

--- model_handler_service/internal/handlers/features_handler.go
+++ model_handler_service/internal/handlers/features_handler.go
@@ -24,24 +24,30 @@ import (
 //	@Failure	400	{object}	app_errors.AppError
 //	@Router		/increase_features [post]
 func (c *CoreHandler) IncreaseFeatures(w http.ResponseWriter, r *http.Request) error {
+	// Объявляем текущую операцию для оборачивания ошибки
 	op := "handlers.CoreHandler.IncreaseFeatures"
+	// Берем логгер из контекста
 	l := logger.EntryWithRequestIDFromContext(r.Context())
 
+	// Десереализуем данные из тела запроса
 	var req fixtures.IncreaseFeaturesRequest
-
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		return app_errors.ErrParseError.WrapError(op, err.Error())
 	}
 
+	// Валидируем данные на наличие необходимых полей
 	appErr := customTools.ValidateStruct(c.validator, req)
 	if appErr != nil {
 		return appErr
 	}
 
+	// Делаем все изменения данных в БД в транзакции
 	txErr := c.transactor.WithinTransaction(r.Context(), func(txCtx context.Context) error {
+		// Берем модель пользователя по ID пользователя и типу модели из БД
 		_, err = c.featureRepository.GetModelByUserID(txCtx, req.UserID, data.FaceModel)
 		if err != nil {
+			// Если модель не нашлась, то создаем новоую в БД
 			if app_errors.IsNotFound(err) {
 				err = c.featureRepository.CreateModel(txCtx, req.UserID, data.FaceModel)
 				if err != nil {
@@ -52,6 +58,7 @@ func (c *CoreHandler) IncreaseFeatures(w http.ResponseWriter, r *http.Request) e
 			}
 		}
 
+		// увеличиваем количество признаков на желаемое значение в БД
 		err = c.featureRepository.ChangeFeaturesCount(txCtx, req.UserID, data.FaceModel, req.FeaturesCount)
 		if err != nil {
 			return fmt.Errorf("%s: %w", op, err)
@@ -64,6 +71,7 @@ func (c *CoreHandler) IncreaseFeatures(w http.ResponseWriter, r *http.Request) e
 		return txErr
 	}
 
+	// Возвращаем пустой ответ со статусом 204
 	api.WriteSuccess(r.Context(), w, struct{}{}, http.StatusNoContent, l)
 	return nil
 }
--- model_handler_service/internal/handlers/model_saver.go
+++ model_handler_service/internal/handlers/model_saver.go
@@ -17,13 +17,6 @@ import (
 	"strconv"
 )
 
-type Message struct {
-	ModelType     string `json:"model_type"`
-	UserID        string `json:"user_id"`
-	ModelURL      string `json:"model_url"`
-	FeaturesCount int    `json:"features_count"`
-}
-
 // SaveModel godoc
 //
 //	@Summary	Принимает ml модель
@@ -34,51 +27,65 @@ type Message struct {
 //	@Failure	400	{object}	app_errors.AppError
 //	@Router		/save_model [post]
 func (c *CoreHandler) SaveModel(w http.ResponseWriter, r *http.Request) error {
+	// Объявляем текущую операцию для оборачивания ошибки
 	op := "handlers.CoreHandler.SaveModel"
+	// Берем логгер из контекста
 	l := logger.EntryWithRequestIDFromContext(r.Context())
 
+	// Берем модель из формы по ключу file
 	file, header, err := r.FormFile("file")
 	if err != nil {
 		return err
 	}
+	// Закрываем файл при завершении функции
 	defer func(file multipart.File) {
 		err := file.Close()
 		if err != nil {
 			l.Error(fmt.Errorf("%s: %w", op, err).Error())
 		}
 	}(file)
-
+	// Берем строковое значение user_id из формы
 	userID := r.FormValue("user_id")
+	// Берем строковое значение model_type из формы
 	modelType := r.FormValue("model_type")
+	// Берем строковое значение features_count из формы
 	featuresCountString := r.FormValue("features_count")
 
+	// Преобразуем features_count к типу int
 	featuresCount, err := strconv.Atoi(featuresCountString)
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
+	// Создаем новый uuid для модели
 	modelID, err := uuid.NewUUID()
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
 
+	// Задаем путь и название файла в s3
 	filename := path.Join(modelType, userID, fmt.Sprintf("%s_%s", modelID.String(), header.Filename))
 
+	// Делаем все изменения данных в транзакции
 	txErr := c.transactor.WithinTransaction(r.Context(), func(txCtx context.Context) error {
+		// Задаем новый s3 ключ для модели
 		err := c.featureRepository.SetModelS3Key(txCtx, filename, modelType, userID)
 		if err != nil {
 			return fmt.Errorf("%s: %w", op, err)
 		}
 
+		// Задаем статус модели - обучена
 		err = c.featureRepository.SetModelStatus(txCtx, data.StatusTrained, modelType, userID)
 		if err != nil {
 			return fmt.Errorf("%s: %w", op, err)
 		}
 
+		// Задаем количество признаков, которые использовались в обучении
 		err = c.featureRepository.SetFeaturesCountUsed(txCtx, userID, modelType, featuresCount)
 		if err != nil {
 			return fmt.Errorf("%s: %w", op, err)
 		}
 
+		// Сохраняем модель в s3
 		err = c.modelSaver.SaveFile(r.Context(), filename, file)
 		if err != nil {
 			return fmt.Errorf("%s: %w", op, err)
@@ -90,6 +97,7 @@ func (c *CoreHandler) SaveModel(w http.ResponseWriter, r *http.Request) error {
 		return txErr
 	}
 
+	// Возвращаем пустой ответ со статусом 204
 	api.WriteSuccess(r.Context(), w, struct{}{}, http.StatusNoContent, l)
 	return nil
 }
@@ -104,26 +112,31 @@ func (c *CoreHandler) SaveModel(w http.ResponseWriter, r *http.Request) error {
 //	@Failure	400				{object}	app_errors.AppError
 //	@Router		/get_models [post]
 func (c *CoreHandler) GetModels(w http.ResponseWriter, r *http.Request) error {
+	// Объявляем текущую операцию для оборачивания ошибки
 	op := "handlers.CoreHandler.GetModels"
+	// Берем логгер из контекста
 	l := logger.EntryWithRequestIDFromContext(r.Context())
 
+	// Десереализуем данные из тела запроса
 	var req fixtures.GetModelsRequest
-
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		return app_errors.ErrParseError.WrapError(op, err.Error())
 	}
 
+	// Валидируем данные на наличие необходимых полей
 	appErr := customTools.ValidateStruct(c.validator, req)
 	if appErr != nil {
 		return appErr
 	}
 
+	// Находим модели данного пользователя
 	models, err := c.featureRepository.GetModelsByUserID(r.Context(), req.UserID)
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
 
+	// Формируем ответ, состоящий из предподписанных ссылок на скачивание всех типов моделей
 	modelsURLS := make(map[string]string, len(models))
 	for _, model := range models {
 		if model.S3Key != nil {
@@ -136,6 +149,7 @@ func (c *CoreHandler) GetModels(w http.ResponseWriter, r *http.Request) error {
 		}
 	}
 
+	// Возвращаем результат со статусом 200
 	api.WriteSuccess(r.Context(), w, modelsURLS, http.StatusOK, l)
 	return nil
 }
